ee8fad161baa97112d6576af42b4a27daba88057
Matvei Slavenko <slavenkofm@gmail.com>
2022-03-20 01:47:50 +0100

    shell script

---
+++ rf_cv_karolina_shell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -N rf
+#PBS -l select=1:ncpus=128,walltime=00:50:00
+#PBS -q qexp
+#PBS -e rf_cv.e
+#PBS -o rf_cv.o
+
+pwd
+
+module load R
+echo "loaded R"
+
+time Rscript rf_cv_mc.r 16
